5fe5d116de01a519fc990da7278c05cbb1addedc
Edgar Fernandez <tempurakohi@gmail.com>
2022-03-05 20:07:35 -0800

    did readme

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# E-Commerce
+# E-Commerce Backend
+[ScreenCastify Link](https://drive.google.com/file/d/13aQ3oM5y0Iac82s8N2z0fBG4TApzX9lf/view)
+[GitRepo](https://github.com/HCTyler/E-Commerce)
+
+![Github License](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Description
+
+This is the backend portion of an E-commerce site. This application uses Express.js for its server and MySQL for the database.
+
+## Table of Contents
+
+[Installation](#installation)
+
+[Usage](#usage)
+
+[Credits](#credits)
+
+[License](#license)
+
+## Installation
+
+To install necessary dependencies for this project, run the following command:
+
+> npm install
+
+> mysql -u root -p (exclude -p if no password)
+
+> npm run seed
+
+> npm run start
+
+## Usage
+
+Node.js and MySQL is needed.
+
+## Credits
+
+## License
+
+This project is licensed under the MIT license.
+
+## Questions
+
+If you have any other questions about the repo, contact me at undefined. You can find more of my work at [HCTyler](https://github.com/undefined)
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -58,18 +58,18 @@ router.post('/', (req, res) => {
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length && req.body.tagIds.length) {
-        const productIdArray = req.body.tagIds.map((tag_id) => {
+        const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
             tag_id,
           };
         });
-        return ProductTag.bulkCreate(productIdArray);
+        return ProductTag.bulkCreate(productTagIdArr);
       }
       // if no product tags, just respond
       res.status(200).json(product);
     })
-    .then((productId) => res.status(200).json(productId))
+    .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
@@ -86,13 +86,13 @@ router.put('/:id', (req, res) => {
   })
     .then((product) => {
       if (req.body.tagIds && req.body.tagIds.length){
-        const productItems= ProductTag.findAll({ where: { product_id: req.params.id }
+        const productTags= ProductTag.findAll({ where: { product_id: req.params.id }
         })
 
-      const productId = productItems.map(({ tag_id }) => tag_id);
+      const productTagIds = productTags.map(({ tag_id }) => tag_id);
       // create filtered list of new tag_ids
-      const newProductItems = req.body.tagIds
-        .filter((tag_id) => !productId.includes(tag_id))
+      const newProductTags = req.body.tagIds
+        .filter((tag_id) => !productTagIds.includes(tag_id))
         .map((tag_id) => {
           return {
             product_id: req.params.id,
@@ -100,14 +100,14 @@ router.put('/:id', (req, res) => {
           };
         });
       // figure out which ones to remove
-      const productRemove = productItems
+      const productTagsToRemove = productTags
         .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
         .map(({ id }) => id);
 
       // run both actions
       return Promise.all([
-        ProductTag.destroy({ where: { id: productRemove } }),
-        ProductTag.bulkCreate(newProductItems),
+        ProductTag.destroy({ where: { id: productTagsToRemove } }),
+        ProductTag.bulkCreate(newProductTags),
       ]);
       }
       return res.json(product)
